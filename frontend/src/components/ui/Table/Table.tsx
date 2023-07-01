b3566f1d64fa77a67cc86fbc8592d35cca9d2d8d
Ilia Filimonov <9991571155i@gmail.com>
2023-07-02 01:59:36 +0400

    fix

--- frontend/src/components/ui/Table/Table.tsx
+++ frontend/src/components/ui/Table/Table.tsx
@@ -1,66 +1,71 @@
 import React, { useRef } from 'react';
 import DropdownMenu from '../DropdownMenu/DropdownMenu';
+import { RiEyeLine, RiEdit2Line, RiDeleteBinLine } from 'react-icons/ri';
 
 interface TableProps<T> {
   headers: { key: keyof T; name: string; width?: number }[];
   data: T[];
   onDropdownOptionSelect: (option: string, row: T) => void;
+  emptyMessage?: string;
 }
 
-const Table = <T,>({ data, headers, onDropdownOptionSelect }: TableProps<T>): React.ReactElement => {
+const Table = ({ data, headers, onDropdownOptionSelect, emptyMessage }: TableProps<any>): React.ReactElement => {
   const headsRef = useRef<HTMLTableRowElement>(null);
 
-  const handleDropdownOptionClick = (option: string, row: T) => {
+  const handleDropdownOptionClick = (option: string, row: any) => {
     if (onDropdownOptionSelect) {
       onDropdownOptionSelect(option, row);
     }
   };
 
+  const dropDownItems = [
+    { text: 'Открыть', icon: <RiEyeLine className="text-gray-600 mr-2 w-4" size={18} /> },
+    { text: 'Редактировать', icon: <RiEdit2Line className="text-gray-600 mr-2 w-4" size={18} /> },
+    { text: 'Удалить', icon: <RiDeleteBinLine className="text-gray-600 mr-2 w-4" size={18} /> },
+  ];
+
   return (
-    <>
-      <table className="w-full border-collapse">
-        <thead>
-          <tr ref={headsRef}>
+    <table className="w-full border-collapse">
+      <colgroup>
+        {headers.map((header) => (
+          <col key={String(header.key)} style={{ width: header.width }} />
+        ))}
+      </colgroup>
+      <thead>
+        <tr ref={headsRef}>
+          {headers.map((header) => (
+            <th
+              key={String(header.key)}
+              className="py-2 px-4 font-medium cursor-move bg-blue-200 text-blue-500 border border-blue-500"
+            >
+              {header.name}
+            </th>
+          ))}
+          <th className="py-2 px-4"></th>
+        </tr>
+      </thead>
+      <tbody>
+        {data.map((item, index) => (
+          <tr key={`${index}`}>
             {headers.map((header) => (
-              <th
-                key={String(header.key)}
-                className="py-2 px-4 font-medium cursor-move bg-blue-200 text-blue-500 border border-blue-500"
-                style={{
-                  width: header.width,
-                }}
-              >
-                {header.name}
-              </th>
-            ))}
-            <th className="py-2 px-4"></th>
-          </tr>
-        </thead>
-        <tbody>
-          {data.length ? (
-            data.map((item, index) => (
-              <tr key={`${index}_${String(item[headers[0].key])}`}>
-                {headers.map((header) => (
-                  <td key={`${String(header.key)}_${String(item[headers[0].key])}`} className="py-2 px-4 border">
-                    {String(item[header.key])}
-                  </td>
-                ))}
-                <td className="p-0">
-                  <DropdownMenu
-                    onOptionSelect={(option) => handleDropdownOptionClick(option, item)}
-                  />
-                </td>
-              </tr>
-            ))
-          ) : (
-            <tr>
-              <td className="py-2 px-4 text-center border" colSpan={headers.length + 1}>
-                No students available
+              <td key={`${String(header.key)}_${index}`} className="py-2 px-4 border">
+                {String(item[header.key])}
               </td>
-            </tr>
-          )}
-        </tbody>
-      </table>
-    </>
+            ))}
+            <td className="p-0">
+              <DropdownMenu items={dropDownItems} onOptionSelect={(option) => handleDropdownOptionClick(option, item)} />
+            </td>
+          </tr>
+        ))}
+        {data.length === 0 && (
+          <tr>
+            <td className="py-2 px-4 text-center border" colSpan={headers.length + 1}>
+              {emptyMessage || 'Таблица пуста'}
+            </td>
+          </tr>
+        )}
+      </tbody>
+    </table>
   );
 };
 
