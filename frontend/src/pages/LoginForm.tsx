5abcf2fbc95cf3f2eab2b580695a319dfeb8b389
Ilia Filimonov <9991571155i@gmail.com>
2023-07-31 14:42:05 +0400

    auth store (login/registration forms)

--- frontend/src/pages/LoginForm.tsx
+++ frontend/src/pages/LoginForm.tsx
@@ -1,58 +1,33 @@
-import React, { useState } from 'react';
-import axios from 'axios';
-import { useRouter } from 'next/router';
-import Cookies from 'js-cookie';
+import React, { useState } from "react";
+import { observer } from "mobx-react-lite";
+import authStore from "@/store/AuthStore/AuthStore";
 
-interface Tokens {
-  access_token: string;
-  refresh_token: string;
-}
 
-interface AuthDto {
-  username: string;
-  password: string;
-}
-
-const LoginForm: React.FC = () => {
-  const [username, setUsername] = useState<string>('');
-  const [password, setPassword] = useState<string>('');
-
-  const router = useRouter();
+const LoginForm: React.FC = observer(() => {
+  const [username, setUsername] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
 
   const handleLogin = async (e: React.MouseEvent<HTMLButtonElement>) => {
     e.preventDefault();
-
-    try {
-      const dto: AuthDto = { username, password };
-      const response = await axios.post<Tokens>('http://localhost:3000/auth/local/signin', dto);
-      const tokens = response.data;
-
-      console.log('accessToken:', tokens.access_token);
-      console.log('refreshToken:', tokens.refresh_token);
-
-      Cookies.set('accessToken', tokens.access_token);
-      Cookies.set('refreshToken', tokens.refresh_token);
-
-      router.push('/');
-    } catch (error) {
-      console.error('Error during login:', error);
-    }
+    await authStore.login(username, password);
   };
 
   return (
     <div>
       <h2>Login form</h2>
-      <div>
-        <label>Username:</label>
-        <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} />
-      </div>
-      <div>
-        <label>Password:</label>
-        <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
-      </div>
-      <button onClick={handleLogin}>Login</button>
+      <form>
+        <div>
+          <label>Username:</label>
+          <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} />
+        </div>
+        <div>
+          <label>Password:</label>
+          <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+        </div>
+        <button onClick={handleLogin}>Login</button>
+      </form>
     </div>
   );
-};
+});
 
 export default LoginForm;
