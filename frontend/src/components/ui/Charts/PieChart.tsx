40966fe283db5dbbe4dbdfb41c228a34559687e3
Dinar <pravovoy116@gmail.com>
2023-08-03 23:05:19 +0300

    testPieChart

---
+++ frontend/src/components/ui/Charts/PieChart.tsx
@@ -0,0 +1,63 @@
+import React from 'react';
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+  ChartData,
+  CoreChartOptions,
+} from 'chart.js';
+import { Bar } from 'react-chartjs-2';
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+);
+
+const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+
+export type ChartItem = { label: string; size: number };
+
+interface Props {
+  data:ChartItem[];
+  maxSize?: number;
+}
+
+const PieChart: React.FC<Props> = ({ data, maxSize }) => {
+  const options: CoreChartOptions<'bar'> = {
+    responsive: true,
+  };
+
+  const preparedData: ChartData<'bar'> = {
+    labels,
+    datasets: [
+      {
+        label: 'Dataset 1',
+        data: labels.map((_, i) => i + 1),
+        backgroundColor: 'rgba(255, 99, 132, 0.5)',
+      },
+      {
+        label: 'Dataset 2',
+        data: data
+          .map((i) => maxSize || Math.max(...data.map((i) => i.size)) - i.size),
+        backgroundColor: 'rgba(53, 162, 235, 0.5)',
+      },
+    ],
+  };
+
+  return (
+    <Bar
+      data={preparedData}
+      options={options}
+    />
+  );
+};
+
+export default PieChart;
--- frontend/src/components/ui/Table/Table.tsx
+++ frontend/src/components/ui/Table/Table.tsx
@@ -13,40 +13,40 @@ interface TableProps<T> {
   emptyMessage?: string;
 }
 
-const Table = <T extends object>({ data, headers, onDropdownOptionSelect, emptyMessage }: TableProps<T>): React.ReactElement => {
-  return (
-    <div className="w-full">
-      <div className="flex flex-row">
-        {headers.map((header) => (
-          <div
-            key={String(header.key)}
-            className="py-2 px-4 font-medium cursor-move bg-blue-200 text-blue-500 border border-blue-500"
-          >
-            {header.name}
-          </div>
-        ))}
-        <div className="py-2 px-4 flex-grow"></div>
-      </div>
-      {data.length > 0 ? (
-        data.map((item, index) => (
-          <TableRow<T>
-            key={`${index}`}
-            rowData={item}
-            onDropdownOptionSelect={onDropdownOptionSelect}
-          />
-        ))
-      ) : (
-        <div className="flex flex-row">
-          <div
-            className="py-2 px-4 text-center border"
-            style={{ gridColumn: `1 / span ${headers.length + 1}` }}
-          >
-            {emptyMessage || 'Таблица пуста'}
-          </div>
+const Table = <T extends object>({
+  data, headers, onDropdownOptionSelect, emptyMessage,
+}: TableProps<T>): React.ReactElement => (
+  <div className="w-full">
+    <div className="flex flex-row">
+      {headers.map((header) => (
+        <div
+          key={String(header.key)}
+          className="py-2 px-4 font-medium cursor-move bg-blue-200 text-blue-500 border border-blue-500"
+        >
+          {header.name}
         </div>
-      )}
+      ))}
+      <div className="py-2 px-4 flex-grow" />
     </div>
-  );
-};
+    {data.length > 0 ? (
+      data.map((item, index) => (
+        <TableRow<T>
+          key={`${index}`}
+          onDropdownOptionSelect={onDropdownOptionSelect}
+          rowData={item}
+        />
+      ))
+    ) : (
+      <div className="flex flex-row">
+        <div
+          className="py-2 px-4 text-center border"
+          style={{ gridColumn: `1 / span ${headers.length + 1}` }}
+        >
+          {emptyMessage || 'Таблица пуста'}
+        </div>
+      </div>
+    )}
+  </div>
+);
 
 export default Table;
