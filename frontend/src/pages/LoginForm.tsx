9e5b89e978e6b8546565c9cc08f96ac227a99b95
Ilia Filimonov <9991571155i@gmail.com>
2023-08-05 21:16:41 +0400

    fix structure

--- frontend/src/pages/LoginForm.tsx
+++ frontend/src/pages/LoginForm.tsx
@@ -1,31 +1,49 @@
-import React, { useState } from "react";
-import { observer } from "mobx-react-lite";
-import authStore from "@/store/AuthStore/AuthStore";
-
+import React, { useState } from 'react';
+import { observer } from 'mobx-react-lite';
+import { useRouter } from 'next/router';
+import authStore from '@/store/AuthStore/AuthStore';
+import Input from '@/components/ui/Input/Input';
+import Button from '@/components/ui/Button/Button';
 
 const LoginForm: React.FC = observer(() => {
-  const [username, setUsername] = useState<string>("");
-  const [password, setPassword] = useState<string>("");
+  const [username, setUsername] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+
+  const router = useRouter();
 
   const handleLogin = async (e: React.MouseEvent<HTMLButtonElement>) => {
     e.preventDefault();
     await authStore.login(username, password);
+    router.push('/');
   };
 
   return (
-    <div>
-      <h2>Login form</h2>
-      <form>
-        <div>
-          <label>Username:</label>
-          <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} />
-        </div>
-        <div>
-          <label>Password:</label>
-          <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
-        </div>
-        <button onClick={handleLogin}>Login</button>
-      </form>
+    <div className="min-h-screen flex items-center justify-center bg-gray-100 absolute top-0 left-0 right-0">
+      <div className="bg-white p-8 shadow-lg rounded-md w-96">
+        <h2 className="text-2xl font-bold mb-4">Login form</h2>
+        <form>
+          <div className="mb-4">
+            <Input
+              label="Your login"
+              onChange={(e) => setUsername(e)}
+              value={username}
+            />
+          </div>
+          <div className="mb-4">
+            <Input
+              label="Your password"
+              onChange={(e) => setPassword(e)}
+              value={password}
+            />
+          </div>
+          <Button
+            onClick={handleLogin}
+            variant="primary"
+          >
+            Login
+          </Button>
+        </form>
+      </div>
     </div>
   );
 });
--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,13 +1,13 @@
 import '../styles/globals.css';
 import React, { useEffect } from 'react';
 import { observer } from 'mobx-react-lite';
+import { AppProps } from 'next/app';
 import MainLayout from '../Layouts/MainLayout';
 import AuthLayout from '../Layouts/AuthLayout';
-import { AppProps } from 'next/app';
-import authStore from "../store/AuthStore/AuthStore"; // Import the authStore instance directly
+import authStore from '../store/AuthStore/AuthStore';
 
 const App = ({ Component, pageProps }: AppProps) => {
-  const isAuthenticated = authStore.isAuthenticated;
+  const { isAuthenticated } = authStore;
 
   const PageWrapper = isAuthenticated ? MainLayout : AuthLayout;
 
