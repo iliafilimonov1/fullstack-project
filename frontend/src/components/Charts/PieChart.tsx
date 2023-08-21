3d5fe6200bda6f135cfcf50f87ae98d75a3daad9
Dinar <pravovoy116@gmail.com>
2023-08-21 23:02:48 +0300

    correction

--- frontend/src/components/Charts/PieChart.tsx
+++ frontend/src/components/Charts/PieChart.tsx
@@ -10,21 +10,15 @@ interface Props {
   data: { label: string; count: number }[];
 }
 
+const randomColor = () => Math.floor(Math.random() * 16777215).toString(16);
+
 const PieChart: React.FC<Props> = ({ data }) => {
   const localData = {
     labels: data.map((i) => i.label),
     datasets: [
       {
-        label: '# of Votes',
         data: data.map((i) => i.count),
-        backgroundColor: [
-          'rgba(255, 99, 132, 0.2)',
-          'rgba(54, 162, 235, 0.2)',
-          'rgba(255, 206, 86, 0.2)',
-          'rgba(75, 192, 192, 0.2)',
-          'rgba(153, 102, 255, 0.2)',
-          'rgba(255, 159, 64, 0.2)',
-        ],
+        backgroundColor: data.map(() => `#${randomColor()}`),
         borderWidth: 1,
       },
     ],
