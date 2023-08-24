046a6e257931d2ccb75b22ecdab0006746949bc5
Dinar <pravovoy116@gmail.com>
2023-08-24 22:00:50 +0300

    correction

--- frontend/src/components/ui/Alert/Alert.tsx
+++ frontend/src/components/ui/Alert/Alert.tsx
@@ -14,22 +14,22 @@ export interface AlertProps {
   children: React.ReactNode;
 }
 
-const Alert = ({ variant = 'neutral', children }: AlertProps) => {
-  const variantClasses = {
-    neutral: 'bg-gray-100 text-gray-800',
-    info: 'bg-blue-100 text-blue-800',
-    positive: 'bg-green-100 text-green-800',
-    notice: 'bg-yellow-100 text-yellow-800',
-    negative: 'bg-red-100 text-red-800',
-  };
+const variantClasses = {
+  neutral: 'bg-gray-100 text-gray-800',
+  info: 'bg-blue-100 text-blue-800',
+  positive: 'bg-green-100 text-green-800',
+  notice: 'bg-yellow-100 text-yellow-800',
+  negative: 'bg-red-100 text-red-800',
+};
 
-  const iconMapping = {
-    info: <FaInfoCircle className="w-5 h-5" />,
-    positive: <FaCheckCircle className="w-5 h-5" />,
-    notice: <FaExclamationCircle className="w-5 h-5" />,
-    negative: <FaTimesCircle className="w-5 h-5" />,
-  };
+const iconMapping = {
+  info: <FaInfoCircle className="w-5 h-5" />,
+  positive: <FaCheckCircle className="w-5 h-5" />,
+  notice: <FaExclamationCircle className="w-5 h-5" />,
+  negative: <FaTimesCircle className="w-5 h-5" />,
+};
 
+const Alert:React.FC<AlertProps> = ({ variant = 'neutral', children }) => {
   const selectedIcon = iconMapping[variant as keyof typeof iconMapping];
 
   return (
@@ -43,4 +43,6 @@ const Alert = ({ variant = 'neutral', children }: AlertProps) => {
   );
 };
 
-export default Alert;
+Alert.displayName = 'Alert';
+
+export default React.memo(Alert);
