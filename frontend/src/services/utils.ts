a008586f2eabf9e47fc85d4eb088c048bd4c0fef
Dinar <pravovoy116@gmail.com>
2023-09-02 22:47:57 +0300

    просто полезные функции

--- frontend/src/services/utils.ts
+++ frontend/src/services/utils.ts
@@ -1,3 +1,4 @@
+import { AxiosResponse } from 'axios';
 import { ClassValue, clsx } from 'clsx';
 import { twMerge } from 'tailwind-merge';
 
@@ -30,3 +31,72 @@ export function extractStyles(
 export function joinNonEmpty(arr: unknown[], separator: string = ' '): string {
   return arr.filter(Boolean).join(separator);
 }
+
+// Полезные функции которые надо бы сохранить
+/** Функция из swagger, возвращает наименование файла */
+export function extractFileNameFromContentDispositionHeader(value: string) {
+  const patterns = [
+    /filename\*=[^']+'\w*'"([^"]+)";?/i,
+    /filename\*=[^']+'\w*'([^;]+);?/i,
+    /filename="([^;]*);?"/i,
+    /filename=([^;]*);?/i,
+  ];
+
+  let responseFilename = <RegExpExecArray | null>{};
+  patterns.some((regex) => {
+    responseFilename = regex.exec(value);
+    return responseFilename !== null;
+  });
+
+  if (responseFilename !== null && responseFilename.length > 1) {
+    try {
+      return decodeURIComponent(responseFilename[1]);
+    } catch (e) {
+      console.error(e);
+    }
+  }
+  return null;
+}
+
+/** Метод для выгрузки файла */
+export const downloadFile = (response: AxiosResponse) => {
+  const disposition = response.headers['content-disposition'] || response.headers['Content-Disposition'];
+  const responseFileName = extractFileNameFromContentDispositionHeader(disposition);
+
+  const url = URL.createObjectURL(response.data);
+
+  const anchor = document.createElement('a');
+  anchor.href = url;
+  anchor.download = responseFileName ?? 'Документ';
+  anchor.click();
+
+  URL.revokeObjectURL(url);
+};
+
+/** Сериалайзер запроса для Axios */
+// eslint-disable-next-line max-len
+export const paramsSerializer = (request: Record<string, unknown>): string => Object.entries(request)
+  .filter(([key, value]) => {
+    if (value === false) {
+      return !!key;
+    }
+
+    return key && value;
+  })
+  .map(([key, value]) => {
+    if (Array.isArray(value)) {
+      return value.reduce<string>((acc, el, currentIndex) => {
+        if (currentIndex === 0) {
+          return `${key}=${el}`;
+        }
+        return `${acc}&${key}=${el}`;
+      }, '');
+    }
+    return `${key}=${value}`;
+  })
+  .reduce<string>((acc, el, currentIndex) => {
+  if (currentIndex === 0) {
+    return el;
+  }
+  return `${acc}&${el}`;
+}, '');
