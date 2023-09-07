16315b60915e1b10eb7edf8b3f6ee4319709c1ef
Dinar <pravovoy116@gmail.com>
2023-09-07 23:16:07 +0300

    correction

--- frontend/src/components/ui/Checkbox/Checkbox.tsx
+++ frontend/src/components/ui/Checkbox/Checkbox.tsx
@@ -6,7 +6,7 @@ interface Props {
 }
 
 const Checkbox: React.FC<Props> = () => (
-  <div className="w-[10px] h-[10px] p-[1px] rounded-full border-[1px]">
+  <div className="w-[10px] h-[10px] p-[1px] rounded-full border-[1px] flex justify-center items-center">
     <div className="w-full h-full rounded-full bg-red-900" />
   </div>
 );
