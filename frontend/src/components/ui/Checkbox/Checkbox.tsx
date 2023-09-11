6ef057246ef4f138a8d20d1f8872e0835d49753a
Dinar <pravovoy116@gmail.com>
2023-09-11 21:30:24 +0300

    checkbox correction

--- frontend/src/components/ui/Checkbox/Checkbox.tsx
+++ frontend/src/components/ui/Checkbox/Checkbox.tsx
@@ -1,13 +1,21 @@
 import React from 'react';
+import { extractStyles } from '@/services/utils';
 
 interface Props {
-  value?: boolean;
-  onChange?: (value: boolean)=> void;
+  value: boolean;
+  onChange: (value: boolean)=> void;
 }
 
-const Checkbox: React.FC<Props> = () => (
-  <div className="w-[10px] h-[10px] p-[1px] rounded-full border-[1px] flex justify-center items-center">
-    <div className="w-full h-full rounded-full bg-red-900" />
+const Checkbox: React.FC<Props> = ({ onChange, value }) => (
+  <div
+    className="w-[10px] h-[10px] p-[1px] rounded-full border-[1px] flex justify-center items-center"
+    onClick={() => onChange(!value)}
+  >
+    <div className={extractStyles`
+        w-full h-full rounded-full 
+        ${value && 'bg-red-900'}
+    `}
+    />
   </div>
 );
 
